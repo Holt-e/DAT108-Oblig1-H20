f8d97cf215dde1cf716cc5a845fb8e7fbd479835
Holt-e <56269370+Holt-e@users.noreply.github.com>
2020-08-24 13:02:56 +0200

    Oppgave 1 Lagt til sleep to tread på 3 sekunder

--- src/Oppgave1.java
+++ src/Oppgave1.java
@@ -1,20 +1,31 @@
 import javax.swing.JOptionPane;
 public class Oppgave1 {
-public static volatile boolean running = true;
+
+    private String btnString1 = "Ok";
+    private String btnString2 = "Cancel";
+    public static volatile boolean running = true;
 
     public static class RunnableThread implements Runnable {
         @Override
         public void run() {
 
             while(running) {
-                String input = JOptionPane.showInputDialog("Skriv inn din melding, quit for å slutte");
+                try{
+                    Thread.sleep(3000);
+                } catch (InterruptedException e){}
+
+                String stop = "quit";
+                String input = JOptionPane.showInputDialog("Skriv inn din melding, quit for å slutte",stop);
                 System.out.println(input);
+            if(input.equals(stop)){
+                stopThread();
+                }
             }
         }
 
     }
 
-    public void stopThread(){
+    public static void stopThread(){
       boolean running = false;
     }
 }
